544fc0b4755a1ccfddccc12d767ac96986987975
kaushikkallishetty <81616271+kaushikkallishetty@users.noreply.github.com>
2025-04-10 10:53:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-from _utils.spark_session import get_spark_session
-from _utils.s3_utils import read_csv_from_s3
-from _utils.iceberg_utils import write_df
 
-
-def main():
-  spark = get_spark_session()
-  df = read_csv_from_s3(spark)
-  query = write_df(df)
-  query.awaitTermination()
-
-
-if __name__ == "__main__":
-  main()
